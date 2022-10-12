a7d101b7717c3611a03f1b24c9b5f40d87164950
Gabriel <112896989+Gstein77@users.noreply.github.com>
2022-10-12 00:08:36 -0300

    Add files via upload

--- Matrizes Homogeneas/Matrizes.py
+++ Matrizes Homogeneas/Matrizes.py
@@ -1,5 +1,4 @@
-import numpy as np;
-import math;
+import math
 
 class Matrizes:
     def multiplicarMatrizQ(m1,m2):
@@ -19,8 +18,8 @@ class DoisD:
         return [[sx,0,0],[0,sy,0],[0,0,1]]
     
     def getMatrizR(ang):
-        return [[math.cos(ang), -math.sen(ang), 0],
-        [math.sen(ang),math.cos(ang),0],[0,0,1]]
+        return [[math.cos(ang), -math.sin(ang), 0],
+        [math.sin(ang),math.cos(ang),0],[0,0,1]]
 
 class TresD:
     def getMatrizT(tx, ty, tz):
@@ -30,16 +29,16 @@ class TresD:
         return [[sx,0,0,0],[0,sy,0,0],[0,0,sz,0],[0,0,0,1]]
     
     def getMatrizRZ(ang):
-        return [[math.cos(ang), -math.sen(ang), 0,0],
-        [math.sen(ang),math.cos(ang),0,0],[0,0,1,0],[0,0,0,1]]
+        return [[math.cos(ang), -math.sin(ang), 0,0],
+        [math.sin(ang),math.cos(ang),0,0],[0,0,1,0],[0,0,0,1]]
     
     def getMatrizRX(ang):
-        return [[1,0,0,0],[0,math.cos(ang), -math.sen(ang), 0],
-        [0,math.sen(ang),math.cos(ang),0],[0,0,0,1]]
+        return [[1,0,0,0],[0,math.cos(ang), -math.sin(ang), 0],
+        [0,math.sin(ang),math.cos(ang),0],[0,0,0,1]]
     
     def getMatrizRY(ang):
-        return [[math.cos(ang),0, math.sen(ang), 0],
-        [0,1,0,0],[math.sen(ang),0,math.cos(ang),0],[0,0,0,1]]
+        return [[math.cos(ang),0, math.sin(ang), 0],
+        [0,1,0,0],[math.sin(ang),0,math.cos(ang),0],[0,0,0,1]]
     
     def getMatrizRX(b,c,d):
         return [[1,0,0,0],[0,c/d,-b/d,0],[0,b/d,c/d,0],[0,0,0,1]]
@@ -52,54 +51,107 @@ class TresD:
         vu = math.sqrt(v[0]**2+v[1]**2+v[2]**2)
         u = [v[0]/vu,v[1]/vu,v[2]/vu]
         u.append(math.sqrt(v[1]**2+v[2]**2))
-        mx = getMatrizRX(u[1],u[2],u[3])
-        my = getMatrizRY(u[0],u[3])
-        mz = getMatrizRZ(ang)
-        mf = multiplicarMatrizQ(mx,my)
-        mf = multiplicarMatrizQ(mf,mz)
-        mx = getMatrizRX(-u[1],-u[2],-u[3])
-        my = getMatrizRY(-u[0],-u[3])
-        mf = multiplicarMatrizQ(mf,my)
-        mf = multiplicarMatrizQ(mf,mx)
+        mx = self.getMatrizRX(u[1],u[2],u[3])
+        my = self.getMatrizRY(u[0],u[3])
+        mz = self.getMatrizRZ(ang)
+        mf = self.multiplicarMatrizQ(mx,my)
+        mf = self.multiplicarMatrizQ(mf,mz)
+        mx = self.getMatrizRX(-u[1],-u[2],-u[3])
+        my = self.getMatrizRY(-u[0],-u[3])
+        mf = self.multiplicarMatrizQ(mf,my)
+        mf = self.multiplicarMatrizQ(mf,mx)
         return mf
 
-class main:
+class Main:
+
     print("Digite '2' para 2d ou '3' para 3d: ")
-    tam = int(scan())
+    tam = int(input())
     aux = tam+1
-    mf = [[]*aux]*aux
+    mf = [[0]*aux]*aux
     for i in range(aux):
         mf[i][i] = 1
     while aux != 0:
         print("Qual operação deseja realizar?")
         print("0 = Sair\n1 = Translação\n2 = Rotação\n3 = Escala")
         print("4 = Translação Inversa\n5 = Rotação Inversa\n6 = Escala Inversa")
-        aux = int(scan())
+        aux = int(input())
         if aux == 1:
             print("Informe os valores: ")
-            vx = scan()
-            vy = scan()
-            ma = none
+            vx = float(input())
+            vy = float(input())
             if tam == 3:
-                ma = getMatrizT(vx,vy)
+                ma = DoisD.getMatrizT(vx,vy)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
             elif tam == 4:
-                vz = scan()
-                ma = getMatrizT(vx,vy,vz)
-            mf = multiplicarMatrizQ(mf,ma)
+                vz = float(input())
+                ma = TresD.getMatrizT(vx,vy,vz)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
         elif aux == 2:
             print("Informe o ângulo: ")
-            va = scan()
-            ma = none
+            va = float(input())
             if tam == 2:
-                ma = getMatrizR(va)
+                ma = DoisD.getMatrizR(va)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
             elif tam == 3:
                 p1 = []*tam
                 p2 = []*tam
                 print("Informe os valores do ponto 1 do eixo: ")
                 for i in p1:
-                    p1[i] = scan()
+                    p1[i] = float(input())
                 print("Informe os valores do ponto 2 do eixo: ")
                 for i in p2:
-                    p2[i] = scan()
-                ma = getMatrizR(va,p1,p2)
-            mf = mu
+                    p2[i] = float(input())
+                ma = TresD.getMatrizR(va,p1,p2)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
+        elif aux == 3:
+            print("Informe as escalas: ")
+            sx = float(input())
+            sy = float(input())
+            if tam == 3:
+                sz = float(input())
+                ms = TresD.getMatrizS(sx,sy,sz)
+                mf = Matrizes.multiplicarMatrizQ(mf,ms)
+            elif tam == 2:
+                ms = DoisD.getMatrizS(sx,sy)
+                mf = Matrizes.multiplicarMatrizQ(mf,ms)
+        elif aux == 4:
+            print("Informe os valores: ")
+            vx = -float(input())
+            vy = -float(input())
+            if tam == 3:
+                ma = DoisD.getMatrizT(vx,vy)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
+            elif tam == 4:
+                vz = -float(input())
+                ma = TresD.getMatrizT(vx,vy,vz)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
+        elif aux == 5:
+            print("Informe o ângulo: ")
+            va = -float(input())
+            if tam == 2:
+                ma = DoisD.getMatrizR(va)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
+            elif tam == 3:
+                p1 = []*tam
+                p2 = []*tam
+                print("Informe os valores do ponto 1 do eixo: ")
+                for i in p1:
+                    p1[i] = float(input())
+                print("Informe os valores do ponto 2 do eixo: ")
+                for i in p2:
+                    p2[i] = float(input())
+                ma = TresD.getMatrizR(va,p1,p2)
+                mf = Matrizes.multiplicarMatrizQ(mf,ma)
+        elif aux == 6:
+            print("Informe as escalas: ")
+            sx = 1/float(input())
+            sy = 1/float(input())
+            if tam == 3:
+                sz = 1/float(input())
+                ms = TresD.getMatrizS(sx,sy,sz)
+                mf = Matrizes.multiplicarMatrizQ(mf,ms)
+            elif tam == 2:
+                ms = DoisD.getMatrizS(sx,sy)
+                mf = Matrizes.multiplicarMatrizQ(mf,ms)
+    print("Finalizado")
+    print(mf)
